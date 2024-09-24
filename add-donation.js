2bf7dae626726c106217af3c16f69cbff93ac3d9
maahi-hue <nafisakamal180@gmail.com>
2024-09-24 15:55:51 +0600

    added all the js fuctions and transaction history

--- add-donation.js
+++ add-donation.js
@@ -1,3 +1,4 @@
+// Noakhali-donation
 document
   .getElementById("btn-add-donation")
   .addEventListener("click", function (event) {
@@ -9,6 +10,73 @@ document
       const newBalance = balance + addMoney;
 
       document.getElementById("account-balance").innerText = newBalance;
+
+      // add to history
+      const div = document.createElement("div");
+      div.classList.add("border-inherit");
+      div.classList.add("rounded-lg");
+      div.classList.add("border-2");
+      div.classList.add("text-xl");
+      div.innerHTML = `added: ${addMoney} TK. Balance: ${newBalance}`;
+
+      // common function
+      document.getElementById("history-contents").appendChild(div);
+    } else {
+      alert("failed");
+    }
+  });
+
+// feni-donation
+document
+  .getElementById("btn-add-donation2")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoney = getInputFieldValueById("input2");
+
+    if (typeof addMoney === "number" && addMoney > 0) {
+      const balance = getTextFieldValueId("account-balance2");
+      const newBalance = balance + addMoney;
+
+      document.getElementById("account-balance2").innerText = newBalance;
+
+      // add to history
+      const div = document.createElement("div");
+      div.classList.add("border-inherit");
+      div.classList.add("rounded-lg");
+      div.classList.add("border-2");
+      div.classList.add("text-xl");
+      div.innerHTML = `added: ${addMoney} TK. Balance: ${newBalance}`;
+
+      // common function
+      document.getElementById("history-contents").appendChild(div);
+    } else {
+      alert("failed");
+    }
+  });
+
+// quota
+document
+  .getElementById("btn-add-donation3")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoney = getInputFieldValueById("input3");
+
+    if (typeof addMoney === "number" && addMoney > 0) {
+      const balance = getTextFieldValueId("account-balance3");
+      const newBalance = balance + addMoney;
+
+      document.getElementById("account-balance3").innerText = newBalance;
+
+      // add to history
+      const div = document.createElement("div");
+      div.classList.add("border-inherit");
+      div.classList.add("rounded-lg");
+      div.classList.add("border-2");
+      div.classList.add("text-xl");
+      div.innerHTML = `added: ${addMoney} TK. Balance: ${newBalance}`;
+
+      // common function
+      document.getElementById("history-contents").appendChild(div);
     } else {
       alert("failed");
     }
--- main-balance.js
+++ main-balance.js
@@ -1,3 +1,4 @@
+// noakhali
 document
   .getElementById("btn-add-donation")
   .addEventListener("click", function (event) {
@@ -6,7 +7,43 @@ document
 
     if (typeof addMoney === "number" && addMoney > 0) {
       const mainBalance = getTextFieldValueId("main-balance");
-      console.log(mainBalance);
+
+      const newMainBalance = mainBalance - addMoney;
+
+      document.getElementById("main-balance").innerText = newMainBalance;
+    } else {
+      alert("failed");
+    }
+  });
+
+// feni
+document
+  .getElementById("btn-add-donation2")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoney = getInputFieldValueById("input2");
+
+    if (typeof addMoney === "number" && addMoney > 0) {
+      const mainBalance = getTextFieldValueId("main-balance");
+
+      const newMainBalance = mainBalance - addMoney;
+
+      document.getElementById("main-balance").innerText = newMainBalance;
+    } else {
+      alert("failed");
+    }
+  });
+
+// quota
+document
+  .getElementById("btn-add-donation3")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const addMoney = getInputFieldValueById("input3");
+
+    if (typeof addMoney === "number" && addMoney > 0) {
+      const mainBalance = getTextFieldValueId("main-balance");
+
       const newMainBalance = mainBalance - addMoney;
 
       document.getElementById("main-balance").innerText = newMainBalance;
