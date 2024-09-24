6594cad957ee71f74cd313beef87c4d4ce2a0254
maahi-hue <nafisakamal180@gmail.com>
2024-09-24 15:00:55 +0600

    fixed history and index pages

---
+++ features.js
@@ -0,0 +1,6 @@
+document.getElementById("index").addEventListener("click", function () {
+  showSectionById("index-section");
+});
+document.getElementById("history").addEventListener("click", function () {
+  showSectionById("history-section");
+});
--- utilities.js
+++ utilities.js
@@ -8,3 +8,8 @@ function getTextFieldValueId(id) {
   const textNumber = parseFloat(textValue);
   return textNumber;
 }
+function showSectionById(id) {
+  document.getElementById("index-section").classList.add("hidden");
+  document.getElementById("history-section").classList.add("hidden");
+  document.getElementById(id).classList.remove("hidden");
+}
